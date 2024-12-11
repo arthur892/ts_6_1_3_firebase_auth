2346dcc93e644a9824e19ad6ce33046644e56290
arthur <rybalkin.arthur@gmail.com>
2024-12-11 11:06:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ts_6_1_3_firebase_auth
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/ts_6_1_3_firebase_auth/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ts_6_1_3_firebase_auth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
